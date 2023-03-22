3990282ba1950c7aa4f68cd0856fe98976621355
kiyomin0615 <moeday1010@gmail.com>
2023-03-22 14:07:42 +0900

    길찾기 알고리즘 BFS

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // 0 is block, 1 is empty, 2 is player
-let maze = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],];
+let maze = [];
+let found = [];
+let visited = [];
+let parents = [];
 
+const deltaY = [-1, 0, 1, 0];
+const deltaX = [0, 1, 0, -1];
+
+// 좌표 객체
 class Pos {
-  constructor(x, y) {
+  constructor(y, x) {
     this.x = x;
     this.y = y;
   }
 }
 
+// 큐 객체
 class Queue {
   constructor() {
     this.arr = [];
@@ -25,8 +33,10 @@ class Queue {
 
 const buttonElement = document.querySelector("button");
 
+// 버튼 '클릭'하면 게임 시작
 buttonElement.addEventListener("click", gameStart);
 
+// 게임 시작 함수
 function gameStart() {
   resetMaze();
 
@@ -37,14 +47,35 @@ function gameStart() {
   renderMaze();
 }
 
+// 2차원 배열 생성 함수
+function createArray2d(row, col) {
+  let array2d = new Array(row);  
+ 
+	for(let i = 0; i < col; i++) { 
+		array2d[i] = new Array(col);  
+	} 
+ 
+	return array2d; 
+}
+
+// 미로 초기화 함수
 function resetMaze() {
+  maze = createArray2d(15, 15);
+  found = createArray2d(15, 15);
+  visited = createArray2d(15, 15);
+  parents = createArray2d(15, 15);
+
   for (let y = 0; y < 15; y++) {
     for (let x = 0; x < 15; x++) {
       maze[y][x] = 0;
+      found[y][x] = false;
+      visited[y][x] = false;
+      parents[y][x] = new Pos(-1, -1);
     }
   }
 }
 
+// 랜덤 미로 생성 함수
 function generateRandomMaze() {
   // 1. 미로를 막는다
   for (let y = 0; y < 15; y++) {
@@ -87,6 +118,7 @@ function generateRandomMaze() {
   }
 }
 
+// 미로 렌더링 함수
 function renderMaze() {
   for (let y = 0; y < 15; y++) {
     for (let x = 0; x < 15; x++) {
@@ -103,79 +135,82 @@ function renderMaze() {
   }
 }
 
+// BFS 알고리즘
 function BFS() {
-  let playerPos = new Pos(1, 1);
-  let posY = playerPos.y;
-  let posX = playerPos.x;
+  let playerPosition = new Pos(1, 1);
+  let positionY = playerPosition.y;
+  let positionX = playerPosition.x;
 
-  const q = new Queue();
-
-  const deltaY = [-1, 0, 1, 0];
-  const deltaX = [0, 1, 0, -1];
-
-  // javascript에서 배열 선언 방식을 모르겠다
-  const found = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],];
-  const visited = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],];
-  const parents = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],];
+  let q = new Queue();
 
   // 시작 좌표 발견 및 예약
-  found[posY][posX] = true;
-  q.enqueue(new Pos(posY,posX));
-  parents[posY][posX] = new Pos(posY,posX);
+  found[positionY][positionX] = true; // 발견
+  q.enqueue(new Pos(positionY, positionX)); // 예약
+  parents[positionY][positionX] = new Pos(positionY, positionX); // 시작 좌표의 부모는 시작 좌표
 
   while (true) {
-    console.log(q.arr)
+    // 예약한 좌표가 더이상 없으면 종료
     if (q.length() <= 0) {
       break;
     }
-    // 좌표 방문
-    let now = q.dequeue();
-    visited[now.y][now.x] = true;
 
+    // 예약 좌표 방문
+    let nowPosition = q.dequeue();
+    visited[nowPosition.y][nowPosition.x] = true;
+
+    // 다음 좌표 후보
     for (let i = 0; i < 4; i++) {
-      let next = new Pos(now.y+deltaY[i], now.x+deltaX[i]);
+      let nextPosition = new Pos(nowPosition.y + deltaY[i], nowPosition.x + deltaX[i]);
 
-      if (maze[next.y][next.x] === 0) {
+      if (maze[nextPosition.y][nextPosition.x] === 0) {
         continue;
       }
-      if (found[next.y][next.x] === true) {
+      if (found[nextPosition.y][nextPosition.x] === true) {
         continue;
       }
 
       // 다음 좌표 발견 및 예약
-      found[next.y][next.x] = true;
-      q.enqueue(next);
+      found[nextPosition.y][nextPosition.x] = true;
+      q.enqueue(nextPosition);
+      parents[nextPosition.y][nextPosition.x] = nowPosition;
     }
   }
-  // shortestPath();
+
+  shortestPath();
 }
 
-const pathPoints = [];
-
+// 최단 경로 함수
 function shortestPath() {
   // 도착 좌표
   let y = 13;
   let x = 13;
 
-  pathPoints.push(y, x);
+  const points = [];
+
+  points.push(new Pos(y, x));
+
+  for (let y = 0; y < 15; y++) {
+    for (let x = 0; x < 15; x++) {
+      console.log(new Pos(y,x), parents[y][x]);
+    }
+  }
 
   while (true) {
-    // 출발 좌표
     if (parents[y][x].y === y && parents[y][x].x === x) {
       break;
     }
 
-    y = parents[y][x].y;
-    x = parents[y][x].x;
+    y = parents[y][x].y; // 13
+    x = parents[y][x].x; // 12
 
-    pathPoints.push(y, x);
+    points.push(new Pos(y, x));
   }
 
-  pathPoints.push(y, x);
+  points.push(new Pos(y, x));
 
-  pathPoints.reverse();
+  points.reverse();
 
-  for (pathPoint of pathPoints) {
-    maze[pathPoint.y][pathPoint.x] = 2;
+  for (let point of points) {
+    maze[point.y][point.x] = 2;
   }
 }
